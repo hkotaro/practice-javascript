bcdd59da5f46ec6c358ae079d412f6255d79464c
hkotaro <kotaro.1105.0710@gmail.com>
2024-09-19 05:19:32 +0000

    9/19 JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector("h1").textContent="樋口　己太郎"
+document.querySelector("h2").textContent="kotaro"
+//documentとは
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす */
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル（Dokyument Object Model）の略でWedページの要素やコンテンツなどをツリー構造で表現したデータモデル */
+
+//上記コードの意味
+//document.querySelector("h1").textContent="樋口　己太郎"
+
+//ドキュメントの中の（"h1"）タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent="樋口　己太郎"
+
+
