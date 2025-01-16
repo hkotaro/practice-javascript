4fab931a5782c9b08d62d69cab5885f2da98fe47
hkotaro <kotaro.1105.0710@gmail.com>
2025-01-16 05:20:47 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -5,14 +5,35 @@ function ClikCounterGame(){
     let button1=document.createElement("button");
     button1.textContent ="+1ボタン"
 
+    let button2=document.createElement("button");
+    button2.textContent="+10ボタン"
+
+    let button3=document.createElement("button");
+    button3.textContent="リセット"
+
+
     let counter =document.createElement("p")
     counter.textContent =count;
+
     button1.addEventListener("click",function(){
 count++
 counter.textContent=count;
-
 })
+
+button2.addEventListener("click",function(){
+count=count+10
+counter.textContent=count
+})
+
+counter.addEventListener("click",function(){
+    count=0
+    counter.textContent=count
+    })
+
+
 gameContainer.appendChild(button1);
+gameContainer.appendChild(button2);
+gameContainer.appendChild(button3);
 gameContainer.appendChild(counter);
 }
 ClikCounterGame();
